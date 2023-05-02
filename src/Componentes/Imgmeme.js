703c0f66e483920e9d07075e96ae15b5ed6335cc
Rubia <rj-rosales@hotmail.com>
2023-05-01 21:21:22 -0300

    Mejoras en diseño

--- src/Componentes/Imgmeme.js
+++ src/Componentes/Imgmeme.js
@@ -12,6 +12,8 @@ const Imgmeme = () => {
     const [postexto2, setPostexto2] = useState();
     const [colortexto, setColortexto] = useState();
     const [colortexto2, setColortexto2] = useState();
+    // const memeBox = document.querySelector(".memeBox");
+
 
     const textmeme = (e) => {
         setTextomeme(e.target.value);
@@ -67,6 +69,8 @@ const Imgmeme = () => {
     const selectImg = (e) => {
         setImgmeme(e.target.value);
         console.log(e.target.value);
+        // memeBox.classList.remove("hidden");
+
     }
 
 
@@ -90,7 +94,7 @@ const Imgmeme = () => {
                 <form className='d-flex col-md-8 m-1'>
                     <select onChange={selectImg} className="form-select m-1" aria-label="Default select example">
                         <option selected >Selecciona una imagen para tu meme</option>
-                        
+
                         <option value={6}>Futurama</option>
                         <option value={7}>Bob Esponja</option>
                         <option value={8}>Señora</option>
@@ -99,39 +103,41 @@ const Imgmeme = () => {
                 </form>
                 <form className='d-flex col-md-8 m-1 '>
                     <select onChange={selectPostext} className="form-select m-1" aria-label="Default select example">
-                        <option selected>Posición</option>
+                        <option selected>Posición del texto</option>
                         <option value={'up'}>Arriba</option>
                         <option value={'mid'}>Centro</option>
                         <option value={'down'}>Abajo</option>
 
                     </select>
-                    <input onChange={textmeme} className='form-control m-1 ' type='text' placeholder='Escibí tu frase' />
-                    <input onChange={colortext} type='color'name='Color de texto' className='colorInput'/>
+                    <input onChange={textmeme} className='form-control m-1 ' type='text' placeholder='Escribí tu frase y selecciona un color' />
+                    <input onChange={colortext} type='color' name='Color de texto' className='colorInput' />
 
                 </form>
                 <form className='d-flex col-md-8 m-1 '>
                     <select onChange={selectPostext2} className="form-select m-1" aria-label="Default select example">
-                        <option selected>Posición</option>
+                        <option selected>Posición del texto</option>
                         <option value={'up'}>Arriba</option>
                         <option value={'mid'}>Centro</option>
                         <option value={'down'}>Abajo</option>
 
                     </select>
-                    <input onChange={textmeme2} className='form-control m-1 ' type='text' placeholder='Escibí tu frase' />
-                    <input onChange={colortext2} type='color'name='Color de texto' className='colorInput'/>
+                    <input onChange={textmeme2} className='form-control m-1 ' type='text' placeholder='Escribí tu frase y selecciona un color' />
+                    <input onChange={colortext2} type='color' name='Color de texto' className='colorInput' />
                 </form>
 
             </div>
 
-            <figure className='text-center' id='exportar'>
 
-                <img src={`/memesImg/${imgmeme}.jpg`} alt='memes' />
-                <p className={`texto-meme ${postexto}`} style={{"color":`${colortexto}`}} >{textomeme}</p>
-                <p className={`texto-meme ${postexto2}`} style={{"color":`${colortexto2}`}} >{textomeme2}</p>
+
+            <figure className='text-center  memeBox' id='exportar'>
+            
+                <img src={`/memesImg/${imgmeme}.jpg`}  alt="meme"/>
+                <p className={`texto-meme ${postexto}`} style={{ "color": `${colortexto}` }} >{textomeme}</p>
+                <p className={`texto-meme ${postexto2}`} style={{ "color": `${colortexto2}` }} >{textomeme2}</p>
 
             </figure>
 
-            <button className='down-btn' onClick={descarga}>Descargar</button>
+            <button className='down-btn btn btn-light' onClick={descarga}>Descargar</button>
 
         </div>
     )
